027c3b11409b64eeb8317324e0ec5854b5e6c7b8
Jeremy Ellison <jeremy@twotoasters.com>
2010-06-03 11:11:28 -0400

    add client and release build scripts

---
+++ release_build_script.rb
@@ -0,0 +1,38 @@
+#/usr/bin/env ruby
+# This script can be used as a "Run Script" target in XCode to create
+# builds for Two Toasters clients with one click. It generates sane filenames,
+# ensures you are building properly, and creates a zip containing the apps
+# and mobile provisioning profiles needed to install.
+# And finally opens the build directory so you can send it out.
+build_dir = ENV['TARGET_BUILD_DIR']
+platform_name = ENV['PLATFORM_NAME']
+configuration = ENV['CONFIGURATION']
+config_build_dir = ENV['CONFIGURATION_BUILD_DIR']
+project_name = ENV['PROJECT']
+date_string = Time.now.strftime('%m_%d_%Y')
+file_basename = "#{project_name}-#{date_string}"
+
+# Very that we are on the right platform
+if platform_name != 'iphoneos'
+  puts "Cannot generate release build on Simulator. Switch to Device and rebuild"
+  exit 1
+end
+
+# Verify we are on the right configuration
+if configuration != 'Distribution'
+  puts "Cannot generate release build on #{configuration}. Switch to Distribution configuration and rebuild"
+  exit 1
+end
+
+# We are all good, proceed with generating the build
+puts "Compressing builds and provisioning profile..."
+existing_builds = Dir.entries(build_dir).select { |e| e =~ /^#{file_basename}/ }.sort
+last_build_number = existing_builds.empty? ? 0 : existing_builds.last.split('-').last.gsub('.zip', '').to_i
+last_build_number += 1
+filename = "#{file_basename}-#{last_build_number}.zip"
+command = "cd \"#{build_dir}\" && zip -r \"#{filename}\" *.app"
+puts "Executing zip command: #{command}"
+puts `#{command}`
+
+puts "Successfully built #{filename}!"
+`open "#{build_dir}"`
